a066e65db4915c1223f9eeb286bd2cf3275913c9
ingingx <1435965297@qq.com>
2016-12-17 11:18:49 +0800

    cal
    cal

---
+++ pages/me/me.js
@@ -0,0 +1,6 @@
+var initData = 'Author:ingingx\n\n\tqq:145964297\n\ttel:17760339742\n\nfrom:UESTC/SCIE\n\ngithub\nhttps://github.com/ingingX/calculator.git'
+Page({
+  data: {
+    text: initData
+  }
+})
